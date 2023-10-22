c9f70935c43d615b0618a98c583f362d319179fd
Ilyas Temirbayev <mr.temirbay17@gmail.com>
2023-10-23 01:05:37 +0600

    Add redirect for creating task

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -6,26 +6,23 @@ using ToDoApplication.Services.Interfaces;
 
 namespace ToDoApplication.Controllers;
 
-public class TaskController : Controller
-{
+public class TaskController : Controller {
     private readonly ITaskService _taskService;
 
-    public TaskController(ITaskService taskService)
-    {
+    public TaskController(ITaskService taskService) {
         _taskService = taskService;
     }
 
-    public IActionResult Index()
-    {
+    public IActionResult Index() {
         return View();
     }
 
     [HttpPost]
-    [ValidateAntiForgeryToken]
+    //[ValidateAntiForgeryToken]
     public async Task<IActionResult> Create(CreateTaskViewModel model) {
         var response = await _taskService.Create(model);
         if(response.StatusCode == Domain.Enum.StatusCode.OkResult) {
-            return Ok(new { description = response.Description });
+            return Ok(new { description = response.Description, id = response.Data.Id});
         }
         return BadRequest(new { description = response.Description });
     }
